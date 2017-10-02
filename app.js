a2888632fed892c7dd28a0f76bb28211c15af6d2
Manpreet <mpsingh17@outlook.co.nz>
2017-10-02 22:01:41 +1300

    Log in out. protect routing. passport local strategy. membership sysytem done!!

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const passport = require('passport');
+const { ensureAuthenticated } = require('./helpers/auth');
 
 const app = express();
 
+/**************************** Load Routes **************************/
+//ideas routes.
+const ideas = require('./routes/ideas');
+
+//user routes.
+const users = require('./routes/users');
+/**************************** End Load Routes **************************/
+
+/**************************** Configuration ****************************/
+//passport config
+require('./config/passport')(passport);
+
+
+/**************************** End Configuration ************************/
 
 //How middleware works.
 // app.use(function(req, res, next) {
@@ -26,19 +42,17 @@ mongoose.connect('mongodb://localhost/vidjot-dev', {
 .then(() => { console.log('mongoDB connected'); })
 .catch(err => console.log(err));
 
-//Load Idea Model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
-
-/************** Midllewares **************/
-
+/**************************************** Midllewares ******************************/
 //Hanldebars middleware.
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //Body Parser middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//set static folder for express.
+app.use(express.static(path.join(__dirname, 'public')));
 
 //method-override middleware
 app.use(methodOverride('_method'));
@@ -50,18 +64,23 @@ app.use(session({
     saveUninitialized: true
 }));
 
+//passport middlewares. Must come after above mentioned session middleware.
+app.use(passport.initialize());
+app.use(passport.session());
+
 //connect-flash middleware
 app.use(flash());
 
-//global variables for messages. Middleware!!
+//global variables for application.
 app.use(function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
-/************** End Midllewares **************/
+/************************************* End Midllewares ******************************/
 
 //Index request GET
 app.get('/', (req, res) => {
@@ -76,92 +95,14 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-//list of ideas GET.
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-    .sort({date: 'desc'})
-    .then(ideas => {
-        res.render('ideas/index', {
-            ideas: ideas
-        });
-    });
-});
+/**************************** Using routes *********************************/
+//using idea routes
+app.use('/ideas', ensureAuthenticated, ideas)
 
-//Add idea form GET
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-});
+//using user routes
+app.use('/users', users);
 
-//Edit idea form GET
-app.get('/ideas/edit/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-    .then(idea => {
-        res.render('ideas/edit', {
-            idea: idea
-        });
-    });
-});
-
-//Process form data for ideas.
-app.post('/ideas', (req, res) => {
-    let errors = [];
-
-    if (!req.body.title) {
-        errors.push({text: 'Please provide title.'});
-    }
-
-    if (!req.body.details) {
-        errors.push({ text: 'Please provide details for your idea.' });
-    }
-
-    if (errors.length > 0) {
-        res.render('ideas/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-    } else {
-        const newIdea = {
-            title: req.body.title,
-            details: req.body.details
-        }
-        new Idea(newIdea)
-            .save()
-            .then(idea => {
-                req.flash('success_msg', 'Video idea added');
-                res.redirect('/ideas');
-            });
-    }
-});
-
-//Process Edit form PUT.
-app.put('/ideas/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-    .then(idea => {
-        //new values
-        idea.title = req.body.title;
-        idea.details = req.body.details;
-
-        idea.save()
-        .then(idea => {
-            req.flash('success_msg', 'Video idea updated');
-            res.redirect('/ideas');
-        });
-    });
-});
-
-//Delete idea DELETE
-app.delete('/ideas/:id', (req, res) => {
-    Idea.remove({_id: req.params.id})
-    .then(() => {
-        req.flash('success_msg', 'Video idea deleted');
-        res.redirect('/ideas');
-    });
-});
+/**************************** End Using routes *********************************/
 
 const port = 5000;
 app.listen(port, () => {
---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+//Load user model
+const User = mongoose.model('users');
+
+module.exports = function(passport) {
+    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+        User.findOne({
+            email: email
+        })
+        .then((user) => {
+            // check if user exist
+            if(!user) {
+                return done(null, false, {message: 'No user found with this email'});
+            }
+
+            //compare passwords
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if(err) throw err;
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, { message: 'password incorrect!' });
+                }
+            });
+        });
+    }));
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+}
